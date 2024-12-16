eed0a0129c9c0284f89ad68612e5e9462ff72bb3
Felix <felixtrapper147@gmail.com>
2024-12-16 21:58:00 +0300

    Initial commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ay0fg7!z%_$dr^i^c-v+y3^6c-*l2l0o7pdiw+7p+l11sif=5o'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
--- shop/stk_push.py
+++ shop/stk_push.py
@@ -7,7 +7,7 @@ def get_access_token():
     consumer_key = 'r3UfevFwKSjvBdotAqRRqNJtLFhQLjL3GwOb9PMD1nPb6rls'
     consumer_secret = 'eBGGNcGQU9d2lVAAj79CkgNpKGuIpQHGsIBAROUiYJXNtqlZELgL8jTQlHI0LiTb'
     auth_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-    
+
     try:
         response = requests.get(auth_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
         response.raise_for_status()  # Raise an exception for bad status codes
@@ -26,14 +26,14 @@ def initiate_stk_push(phone_number, amount):
     passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
     timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     password = base64.b64encode(f"{business_shortcode}{passkey}{timestamp}".encode()).decode()
-    
+
     stk_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
-    
+
     headers = {
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "application/json"
     }
-    
+
     payload = {
         "BusinessShortCode": business_shortcode,
         "Password": password,
@@ -43,7 +43,7 @@ def initiate_stk_push(phone_number, amount):
         "PartyA": phone_number,
         "PartyB": business_shortcode,
         "PhoneNumber": phone_number,
-        "CallBackURL": "https://np42596z-5000.euw.devtunnels.ms/mpesa-callback",  # Change this to your callback URL
+        "CallBackURL": "https://b9769khp-8000.euw.devtunnels.ms/mpesa-callback",  # Change this to your callback URL
         "AccountReference": "TestPayment",
         "TransactionDesc": "Payment for goods"
     }
@@ -60,6 +60,7 @@ def initiate_stk_push(phone_number, amount):
 # Test the function if run directly
 if __name__ == "__main__":
     # Test with the same values from original script
-    test_phone = "254758680860"
+    test_phone = "254714213413"
     test_amount = "100"
     print(initiate_stk_push(test_phone, test_amount))
+
