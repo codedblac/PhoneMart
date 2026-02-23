f9a392d939061a5ad66aa45e477fd05506980b53
CODED BLACK <codedb693@gmail.com>
2026-02-23 10:24:12 +0300

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -12,7 +12,6 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ["category"]
     search_fields = ["name", "description"]
 
-
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ["user", "product", "quantity", "created_at"]
